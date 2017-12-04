e32aa0d2419695b7d0056b67abeafea58901777d
Unknown <jgbpercy@gmail.com>
2017-12-04 12:13:13 +0000

    Vector Extension tests

---
+++ Assets/Editor/Tests/AssertExt.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using GrimoireTD.Technical;
+using UnityEngine;
+
+namespace GrimoireTD.Tests
+{
+    public static class AssertExt
+    {
+        public static void Approximately(float expected, float actual)
+        {
+            Assert.True(CustomMath.Approximately(expected, actual), "Exp: " + expected + "\nGot: " + actual);
+        }
+
+        public static void Approximately(Vector2 expected, Vector2 actual)
+        {
+            Assert.True(CustomMath.Approximately(expected, actual), "Exp: " + expected.ToString("G4") + "\nGot: " + actual.ToString("G4"));
+        }
+
+        public static void Approximately(Vector3 expected, Vector3 actual)
+        {
+            Assert.True(CustomMath.Approximately(expected, actual), "Exp: " + expected.ToString("G4") + "\nGot: " + actual.ToString("G4"));
+        }
+
+        public static void Approximately(Vector4 expected, Vector4 actual)
+        {
+            Assert.True(CustomMath.Approximately(expected, actual), "Exp: " + expected.ToString("G4") + "\nGot: " + actual.ToString("G4"));
+        }
+    }
+}
---
+++ Assets/Editor/Tests/Map/VectorExtensionTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using UnityEngine;
+using GrimoireTD.Map;
+
+namespace GrimoireTD.Tests.VectorExtensionTests
+{
+    public class VectorExtensionTests
+    {
+        [TestCase(0f, 0f, 0, 0)]
+        [TestCase(0f, 0.49f, 0, 0)]
+        [TestCase(0.4f, 0.49f, 0, 1)]
+        [TestCase(0.4f, -0.25f, 0, 0)]
+        [TestCase(0 - .3f, -0.3f, 0, 0)]
+        public void Vector2ToCoord_Always_ReturnsCorrectCoord(
+            float vectorX,
+            float vectorY,
+            int expectedCoordX,
+            int expectedCoordY)
+        {
+            var vector = new Vector2(vectorX, vectorY);
+
+            var expectedCoord = new Coord(expectedCoordX, expectedCoordY);
+
+            var result = vector.ToCoord();
+
+            Assert.AreEqual(expectedCoord, result);
+        }
+
+        [TestCase(0f, 0f, 0, 0)]
+        [TestCase(0f, 0.49f, 0, 0)]
+        [TestCase(0.4f, 0.49f, 0, 1)]
+        [TestCase(0.4f, -0.25f, 0, 0)]
+        [TestCase(0 - .3f, -0.3f, 0, 0)]
+        public void Vector3ToCoord_Always_ReturnsCorrectCoord(
+            float vectorX,
+            float vectorY,
+            int expectedCoordX,
+            int expectedCoordY)
+        {
+            var vector = new Vector3(vectorX, vectorY, 0f);
+
+            var expectedCoord = new Coord(expectedCoordX, expectedCoordY);
+
+            var result = vector.ToCoord();
+
+            Assert.AreEqual(expectedCoord, result);
+        }
+    }
+}
